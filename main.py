08d99ad5e9bacaa93e2fa6e0a0546368a8537c8e
angelo <a-k.s@live>
2024-05-01 18:42:13 +0200

    programme fonctionnel, objectif : ne pas afficher le resultat si erreur

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 def main():
-
+    
     nombre1 = input("Nombre #1 : ")
     nombre2 = input("Nombre #2 : ")
     operation = input("Entrez l'operation +, -, * ou / : ")
     resultat = 0
     
     if not nombre1.isnumeric() or not nombre2.isnumeric() :
-        print("Erreur: Les deux valeurs doivent être des nombres entiers") and quit()
+        print("Erreur: Les deux valeurs doivent être des nombres entiers")
     else : 
         nombre1 = int(nombre1)
         nombre2 = int(nombre2)
@@ -26,8 +26,7 @@ def main():
         case _:
             print("Erreur: Vous devez entrer une opération entre +, -, * ou /.")
 
-    if not nombre2 ==0 and not operation =="/" :
-        print(f"Le résultat est {resultat}")
+    print(f"Le résultat est {resultat}")
 
 # Ne touchez pas le code ci-dessous
 if __name__ == "__main__":
