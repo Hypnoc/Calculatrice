04df489608e9470fc69d131cbdfd31cbb713f030
angelo <a-k.s@live>
2024-05-05 19:34:11 +0200

    jeu de pile ou face corrigé

--- pile_face.py
+++ pile_face.py
@@ -1,22 +1,24 @@
 import random 
 
-capital = 0
+capital = 100
 nombre_lancer = 0
 lancer = random.randint(1, 100)
 
 
-for i in range(10000):
+for i in range(100):
     if lancer <= 49:
         capital +=1
         nombre_lancer += 1
-        lancer = random.randint(1, 100)
         print(f"lancer #{nombre_lancer}")
         print(f"résultat du lancer : {lancer}")
         print(f"montant capital : {capital}")
+        print("************************")
+        lancer = random.randint(1, 100)
     else:
         capital -=1
         nombre_lancer += 1
-        lancer = random.randint(1, 100)
         print(f"lancer #{nombre_lancer}")
         print(f"résultat du lancer : {lancer}")
         print(f"montant capital : {capital}")
+        print("************************")
+        lancer = random.randint(1, 100)
